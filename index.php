c4a707a1b585fd21d61f894ce18108f268ae15c8
Pacislavek <pacislavek@gmail.com>
2020-10-16 12:33:33 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<html lang="pl">
+
+<head>
+  <!-- meta -->
+  <title>Web006 - Szablon</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Patryk Kowalski | pacislavek@gmail.com">
+  <meta name="description" content="Szablon strony dla studia kreatywnego.">
+  <meta http-equiv="Cache-control" content="public">
+  <meta name="robots" content="index, follow" />
+  <meta name="keywords" content="" />
+  <!-- og meta -->
+  <meta property="og:title" content="Web006 - Szablon" />
+  <meta property="og:url" content="" />
+  <meta property="og:description" content="Szablon strony dla studia kreatywnego." />
+  <meta property="og:image" content="" />
+  <!-- font -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&family=Roboto:wght@300;400;500;700&family=Pacifico&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+  <!-- css -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/global.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css" />
+  <link rel="stylesheet" type="text/css" href="css/waves.css" />
+</head>
+
+<body data-spy="scroll" data-target="#navbar-spy" data-offset="73">
+
+
+  <header id="header">
+    <div class="container-fluid">
+      <div class="row">
+
+        <div class="col col-lg-6 col-md-12">
+          <div class="header-text-wrapper">
+            <h1 class="header-title">Profesjonalne studio kreatywne</h1>
+            <p class="header-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium hic nemo odit dolores earum, doloremque expedita nisi numquam sapiente ipsum quos aut iste tenetur tempora necessitatibus deserunt quaerat qui omnis?</p>
+            <button class="header-button">Sprawdź</button>
+          </div>
+        </div>
+
+        <div class="waves">
+          <div class="wave wave1"></div>
+          <div class="wave wave2"></div>
+          <div class="wave wave3"></div>
+          <div class="wave wave4"></div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- SCRIPTS -->
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  <script src="js/main.js"></script>
+  <script src="js/smoothscroll.js"></script>
+
+  <script>
+    AOS.init();
+
+  </script>
+</body>
+
+
+</html>
